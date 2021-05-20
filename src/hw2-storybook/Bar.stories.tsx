4498de18c7dc963466643126a617cfbc33abeb46
dvalbot <dvalbot@mail.ru>
2021-05-20 10:33:15 +0300

    hw2 formatting

--- src/hw2-storybook/Bar.stories.tsx
+++ src/hw2-storybook/Bar.stories.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import {Bar} from "./Bar";
-import {action} from "@storybook/addon-actions";
+import { Bar } from "./Bar";
+import { action } from "@storybook/addon-actions";
 
 export default {
-    component: Bar,
-    title: "Bar",
-    argTypes: {
-        name: {
-            control: {type: "text"},
-            defaultValue: "Test"
-        }
-    }
+  component: Bar,
+  title: "Bar",
+  argTypes: {
+    name: {
+      control: { type: "text" },
+      defaultValue: "Test",
+    },
+  },
 };
 
-export const barExample = (args: any) =>
-    <Bar clickMe={action("Clicked")} {...args}/>
+export const barExample = (args: any) => (
+  <Bar clickMe={action("Clicked")} {...args} />
+);
--- src/hw2-storybook/Bar.tsx
+++ src/hw2-storybook/Bar.tsx
@@ -1,12 +1,13 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 
 interface Props {
-    name: string
-    clickMe: () => void
+  name: string;
+  clickMe: () => void;
 }
 
-export const Bar: FC<Props> = ({name, clickMe}) =>
-    <div>
-        <h3>{name}</h3>
-        <button onClick={clickMe}>Click me</button>
-    </div>
+export const Bar: FC<Props> = ({ name, clickMe }) => (
+  <div>
+    <h3>{name}</h3>
+    <button onClick={clickMe}>Click me</button>
+  </div>
+);
