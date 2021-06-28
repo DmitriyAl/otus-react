8288bc3ff42ac268195f9dfe124f12d50a9ae91c
dvalbot <dvalbot@mail.ru>
2021-06-28 10:58:11 +0300

    hw3 easy1

---
+++ src/hw3-typescript/easy1.ts
@@ -0,0 +1,26 @@
+// Задание первого уровня 1
+// В функцию приходит массив состояний заказа и фильтруется
+// Нужно заменить FIXME на тип который вычисляется на освове OrderState
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+const orderStates = [
+  "initial",
+  "inWork",
+  "buyingSupplies",
+  "producing",
+  "fullfilled",
+] as const;
+
+type OrderState = typeof orderStates[number];
+type FilteredOrderState = Exclude<OrderState, "buyingSupplies" | "producing">
+
+export const getUserOrderStates = (orderStates: OrderState[]): FilteredOrderState[] => {
+  const filteredStates = [] as FilteredOrderState[];
+  orderStates.forEach((element) => {
+    if (element !== "buyingSupplies" && element !== "producing") {
+      filteredStates.push(element);
+    }
+  });
+  return filteredStates;
+};
